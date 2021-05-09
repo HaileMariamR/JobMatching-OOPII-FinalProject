dfcf7e75f3786fd986ef64ae6eaf98bb4af1debf
Hailemariam Fikadie <se.hailemariam.fikadie@gmail.com>
2021-05-09 11:22:33 +0300

    Initial Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using JobMatching_OOPII_FinalProject.Models;
+
+namespace JobMatching_OOPII_FinalProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LoginController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Signin()
+        {
+            return View();
+
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobMatching_OOPII_FinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
